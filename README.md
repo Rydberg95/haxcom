1c418b53862362dbe40e6a7326fd0ae803cfd263
Elias Rydberg <rydberg1995@gmail.com>
2018-11-04 21:00:58 +0000

    Adding phpsysinfo and some more

---
+++ README.md
@@ -0,0 +1,13 @@
+# haxcom
+
+This is a project for a website to present my photographs. It will not be pretty in a long time.
+
+TODO:
+
+*Create admin tool to be able to upload images
+
+*Create navigation menu
+
+*Create contact page
+
+*Create solution for presenting images from the server
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+  	<link rel="stylesheet" type="text/css" href="css/shit.css">
+    <title>Rydberg.pro</title>
+
+  </head>
+
+  <body>
+	<a href="phpsysinfo/index.php">phpsysinfo</a>
+    <div id="header"> <h1>Homepage</h1> </div>
+
+    <div id="picwrapper">
+        <img class="pic" src="https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"> </img>
+
+        <img class="pic" src="https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"> </img>
+
+        <img class="pic" src="http://www.pngpix.com/wp-content/uploads/2016/03/Fresh-Brinjal-PNG-Image.png"> </img>
+
+        <img class="pic" src="http://doglovely.com/wp-content/uploads/2012/09/dog.jpg"> </img>
+
+        <img class="pic" src="http://www.pngpix.com/wp-content/uploads/2016/03/Fresh-Brinjal-PNG-Image.png"> </img>
+
+        <img class="pic" src="https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"> </img>
+
+        <img class="pic" src="https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg"> </img>
+
+        <img class="pic" src="http://www.pngpix.com/wp-content/uploads/2016/03/Fresh-Brinjal-PNG-Image.png"> </img>
+      
+    </divv>
+ </body>
+</html>
